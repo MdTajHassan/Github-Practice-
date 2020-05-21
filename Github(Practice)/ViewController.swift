95b7a17d98b2536f41c2ab04edd9bd34d48ea725
Md Taj Hassan <tajhassan0355@gmail.com>
2020-05-21 12:02:13 +0530

    Added a button with red background.

--- Github(Practice)/ViewController.swift
+++ Github(Practice)/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let button = UIButton()
+        button.frame = CGRect(x: 50, y: 50, width: 200, height: 100)
+        button.backgroundColor = #colorLiteral(red: 1, green: 0.07317244262, blue: 0, alpha: 1)
+        button.setTitle("button1", for: .normal)
+        view.addSubview(button)
     }
 
 
